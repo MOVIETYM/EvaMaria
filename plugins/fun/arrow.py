bfa5476b56a142e956a24c90fe354371da4fb763
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-08-19 09:59:35 +0530

    Create arrow.py

---
+++ plugins/fun/arrow.py
@@ -0,0 +1,25 @@
+# codes added by @lallu_tg
+# use with proper credits
+
+from pyrogram import Client, filters
+
+
+# EMOJI CONSTANTS
+DART_E_MOJI = "🎯"
+# EMOJI CONSTANTS
+
+
+@Client.on_message(
+    filters.private & filters.command(["throw", "dart"]) 
+)
+async def throw_dart(client, message):
+    """ /throw an @AnimatedDart """
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=DART_E_MOJI,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
