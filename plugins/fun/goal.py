80934618948c3284f9a9c9f6ca217b6115dc74d5
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-08-19 10:00:55 +0530

    Create goal.py

---
+++ plugins/fun/goal.py
@@ -0,0 +1,24 @@
+# codes added by @PaulWalker_tg
+# use with proper credits
+
+from pyrogram import Client, filters
+
+# EMOJI CONSTANTS
+GOAL_E_MOJI = "⚽"
+# EMOJI CONSTANTS
+
+
+@Client.on_message(
+    filters.private & filters.command(["goal", "shoot"]) 
+)
+async def roll_dice(client, message):
+    """ @Goal """
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=GOAL_E_MOJI,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
